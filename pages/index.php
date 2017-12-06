90152f8b5f761f7811c534f82dc1da56ecd2f72e
Ferry Manders <ferrymanders@users.noreply.github.com>
2017-12-06 11:01:44 +0100

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -6,6 +6,6 @@
   <body>
     <center><h1>Welcome</h1></center><br>
     <center><h2>This is <?php echo $hostname; ?> running PHP <?php echo $phpversion; ?> </h2></center><br>
-    <center><iframe src="info.php" width="400" height="400"></iframe></center><br>
+    <center><iframe src="info.php" width="100%" height="500"></iframe></center><br>
   </body>
 </html>
