040c0997d593694555630bd06db28f27aa3fbbd1
Ferry Manders <ferrymanders@users.noreply.github.com>
2017-12-06 10:58:00 +0100

    initiele incheck

---
+++ pages/index.php
@@ -0,0 +1,11 @@
+<?php
+  $hostname = gethostname();
+  $phpversion = phpversion();
+?>
+<html>
+  <body>
+    <center><h1>Welcome</h1></center><br>
+    <center><h2>This is <?php echo $hostname; ?> running PHP <?php echo $phpversion; ?> </h2></center><br>
+    <center><iframe src="info.php" width="400" height="400"></iframe></center><br>
+  </body>
+</html>
